55038b87701b0bc49b6413e4a1f0980abe6adf49
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-20 15:56:49 +0100

    ADD id

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -11,31 +11,31 @@ router.get('/', (req, res) => {
 
 // show con id////////////////
 
-// router.get('/:id',(req, res) => {
-//     const id = parseInt(req.params.id)
-//     const post = posts.find((el) => el.id === id)
+router.get('/:id',(req, res) => {
+     const id = parseInt(req.params.id)
+     const post = posts.find((el) => el.id === id)
 
-//     if (post){
-//         console.log(`Ecco il post con id: ${id}`)
-//          res.json(post)
-//      } else {
-//         res.json({message: `Post con id: ${id} non trovato`})
+     if (post){
+         console.log(`Ecco il post con id: ${id}`)
+          res.json(post)
+      } else {
+         res.json({message: `Post con id: ${id} non trovato`})
+     }
+ })
+
+
+// // show con slug
+// router.get('/:slug', (req, res) => {
+//     const slug = req.params.slug
+//     const post = posts.find((el) => el.slug === slug)
+
+//     if (post) {
+//         console.log(`Ecco il post con slug: ${slug}`);
+//         res.json(post)
+//     } else {
+//         res.json({ message: `Post con slug ${slug} non trovato` })
 //     }
 // })
-
-
-// show con slug
-router.get('/:slug', (req, res) => {
-    const slug = req.params.slug
-    const post = posts.find((el) => el.slug === slug)
-
-    if (post) {
-        console.log(`Ecco il post con slug: ${slug}`);
-        res.json(post)
-    } else {
-        res.json({ message: `Post con slug ${slug} non trovato` })
-    }
-})
 
 // store 
 router.post('/', (req, res) => {
